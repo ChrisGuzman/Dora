c4ccdcfef473e7f2474071da573be144fb9d1e10
Chris Guzman <cguzman@orderup.com>
2016-03-08 18:25:24 -0500

    Upgrade map box version. Handle errors

--- app/src/main/java/com/chris_guzman/dora/MainActivity.java
+++ app/src/main/java/com/chris_guzman/dora/MainActivity.java
@@ -1,12 +1,23 @@
 package com.chris_guzman.dora;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.annotations.PolylineOptions;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.constants.Style;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.views.MapView;
@@ -16,12 +27,16 @@ import java.util.List;
 
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
+import rx.subjects.BehaviorSubject;
 
 public class MainActivity extends AppCompatActivity {
 
     private MapView mapView;
     private MapboxDM mapboxDM;
     private Geometry mGeometry;
+    private LatLng startingLine = new LatLng(39.403057, -76.601754);
+    private LatLng innerHarbor = new LatLng(39.285762, -76.608490);
+    private BehaviorSubject<LatLng> driverPosObs = BehaviorSubject.create();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,16 +44,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mapView = (MapView) super.findViewById(R.id.mapview);
         mapView.setStyleUrl(Style.MAPBOX_STREETS);
-        LatLng towsonMall = new LatLng(39.403057, -76.601754);
-        LatLng innerHarbor = new LatLng(39.285762, -76.608490);
-        mapView.setCenterCoordinate(towsonMall);
-        mapView.addMarker(new MarkerOptions().position(towsonMall));
+        boolean mock = true;
+        if (mock) {
+            startingLine = new LatLng(39.403057, -76.601754);
+        }
+        else {
+            LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                Log.d("CHRIS", "can't do that");
+                return;
+            }
+            Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            double longitude = location.getLongitude();
+            double latitude = location.getLatitude();
+            startingLine = new LatLng(latitude, longitude);
+        }
+        mapView.setCenterCoordinate(startingLine);
+        mapView.addMarker(new MarkerOptions().position(startingLine));
         mapView.addMarker(new MarkerOptions().position(innerHarbor));
         mapView.setZoomLevel(10);
         mapView.onCreate(savedInstanceState);
         MapboxAPI apiClient = MapboxClient.getClient(this);
         mapboxDM = MapboxDM.init(apiClient);
-        mapboxDM.getDirections(towsonMall, innerHarbor, this.getResources().getString(R.string.access_token))
+        mapboxDM.getDirections(startingLine, innerHarbor, this.getResources().getString(R.string.access_token))
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Action1<MapboxDirections>() {
                     @Override
@@ -59,11 +94,51 @@ public class MainActivity extends AppCompatActivity {
 
     private void onSuccess(MapboxDirections mapboxDirections) {
         if (mapboxDirections != null && mapboxDirections.getRoutes() != null && !mapboxDirections.getRoutes().isEmpty()) {
-            Routes routes = mapboxDirections.getRoutes().get(0);
-            mGeometry = routes.getGeometry();
-            new DrawGeoJSON().execute();
+            Routes route = mapboxDirections.getRoutes().get(0);
+            if (route.getGeometry() != null && !route.getGeometry().getCoordinates().isEmpty()) {
+                mGeometry = route.getGeometry();
+                new DrawGeoJSON().execute();
+                startDriverTrackingForNav(mGeometry.getCoordinates());
+            }
+            else {
+                Toast.makeText(this, "Unexpected error", Toast.LENGTH_LONG).show();
+            }
         }
+        else if (mapboxDirections != null && mapboxDirections.getError() != null) {
+            //Mapbox will return a 200 with an error string if the request is valid, but it is unable to find a route.
+            Toast.makeText(this, mapboxDirections.getError(), Toast.LENGTH_LONG).show();
+        }
+        else {
+            Toast.makeText(this, "Unexpected error", Toast.LENGTH_LONG).show();
+        }
+
     }
+
+    private void startDriverTrackingForNav(final ArrayList coordinates) {
+            Toast.makeText(this, "Getting your location...", Toast.LENGTH_LONG).show();
+            final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            private int idx = 0;
+
+            @Override
+            public void run() {
+                if (idx < coordinates.size()) {
+                    ArrayList<Double> coordinate = (ArrayList) coordinates.get(idx);
+                    CameraPosition cameraPosition = new CameraPosition.Builder()
+                            .zoom(16)
+                            .target(new LatLng(coordinate.get(1), coordinate.get(0)))
+                            .bearing(180)                               // Sets the orientation of the camera to look south
+                            .tilt(85)                                   // Sets the tilt of the camera to 20 degrees
+                            .build();
+
+                    mapView.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 250, null);
+                    idx++;
+                    handler.postDelayed(this, 250);
+                }
+            }
+        }, 2000);
+    }
+
 
     private class DrawGeoJSON extends AsyncTask<Void, Void, List<LatLng>> {
         @Override
--- app/src/main/java/com/chris_guzman/dora/MapboxDirections.java
+++ app/src/main/java/com/chris_guzman/dora/MapboxDirections.java
@@ -6,9 +6,14 @@ import java.util.ArrayList;
  * Copyright (c) 2015 OrderUp. All rights reserved.
  */
 public class MapboxDirections {
+    private String error;
     private ArrayList<Routes> routes;
 
     public ArrayList<Routes> getRoutes() {
         return routes;
+    }
+
+    public String getError() {
+        return error;
     }
 }
